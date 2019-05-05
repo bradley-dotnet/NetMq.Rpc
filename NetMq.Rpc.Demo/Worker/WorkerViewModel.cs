aa543dafe15539cfdfb5c5320cb8d5b8a95dd744
Bradley Carestia <bradley.dotnet@gmail.com>
2019-05-04 22:05:28 -0700

    Create demo project

---
+++ NetMq.Rpc.Demo/Worker/WorkerViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetMq.Rpc.Demo.Broker.Worker
+{
+    class WorkerViewModel
+    {
+    }
+}

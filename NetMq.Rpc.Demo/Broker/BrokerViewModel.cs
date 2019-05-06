54b5471b205457facdeb9f30312ba78eaeaf459c
Bradley Carestia <bradley.dotnet@gmail.com>
2019-05-05 20:49:41 -0700

    Basic broker demo view

--- NetMq.Rpc.Demo/Broker/BrokerViewModel.cs
+++ NetMq.Rpc.Demo/Broker/BrokerViewModel.cs
@@ -1,12 +1,49 @@
-﻿using System;
+﻿using NetMq.Rpc.Contracts;
+using NetMq.Rpc.Demo.Utilities;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace NetMq.Rpc.Demo.Broker
 {
-    class BrokerViewModel
+    class BrokerViewModel : INotifyPropertyChanged
     {
+        private IRpcMajordomo broker;
+        private bool brokerIdle = true;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public BrokerViewModel()
+        {
+            StartCommand = new DelegateCommand(Start);
+        }
+
+        public void Start()
+        {
+            BrokerIdle = false;
+            broker = new RpcMajordomo(Constants.ZeroMqEndpoint);
+        }
+
+        public ICommand StartCommand { get; private set; }
+
+        public bool BrokerIdle
+        {
+            get => brokerIdle;
+            set
+            {
+                brokerIdle = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void OnPropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
---
+++ NetMq.Rpc.Demo/Utilities/DelegateCommand.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace NetMq.Rpc.Demo.Utilities
+{
+    public class DelegateCommand : ICommand
+    {
+        private Func<object, bool> canExecute;
+        private Action<object> execute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute?.Invoke(parameter) ?? true;
+        }
+
+        public void Execute(object parameter)
+        {
+            execute(parameter);
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, new EventArgs());
+        }
+
+        public DelegateCommand(Action execute)
+        {
+            this.execute = (p) => execute();
+        }
+
+        public DelegateCommand(Action<object> execute)
+        {
+            this.execute = execute;
+        }
+
+        public DelegateCommand(Action execute, Func<bool> canExecute)
+        {
+            this.execute = (p) => execute();
+            this.canExecute = (p) => canExecute();
+        }
+
+        public DelegateCommand(Action execute, Func<object, bool> canExecute)
+        {
+            this.execute = (p) => execute();
+            this.canExecute = canExecute;
+        }
+
+        public DelegateCommand(Action<object> execute, Func<bool> canExecute)
+        {
+            this.execute = execute;
+            this.canExecute = (p) => canExecute();
+        }
+
+        public DelegateCommand(Action<object> execute, Func<object, bool> canExecute)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+    }
+}
